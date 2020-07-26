4fc671ac86344286bd60a871050ad66c0f3f9337
Vladimir Romero <vladdromero@gmail.com>
2020-07-25 23:34:22 -0400

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ const express = require('express');
 
 // api
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
 // route - endpoints
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello, world!');
-  });
+// api.get('/', (req, res) => {
+//     console.log(req);
+//     res.send('Hello, world!');
+//   });
+
+//   middleware use
+//   api.use((req, res, next) => {
+//     console.log('Hello');
+//     next();
+//    });
+
